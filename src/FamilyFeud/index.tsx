985bddffec1c2cc2d292bec928a65585597cc4fc
Dom <59603300+TokyoDom@users.noreply.github.com>
2021-07-03 12:21:55 +0900

    game over and play again logic

--- src/FamilyFeud/index.tsx
+++ src/FamilyFeud/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Button from"react-bootstrap/Button";
 import "./index.css";
 
 type answer = {
@@ -51,16 +52,17 @@ const autoRows: gridAutoRows = {
 
 function FamilyFeud() {
   const [guess, setGuess] = useState("");
-  const [board, setBoard] = useState<answer[]>(prompt.answers);
+  const [board, setBoard] = useState<answer[]>(prompt.answers.map(el => ({... el })));
   const [wrongAnswers, setWrongAnswers] = useState<string[]>([]);
+  const [gameOver, setGameOver] = useState(false);
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (wrongAnswers.length >= 3) return;
+    if (gameOver) return;
     setGuess(e.target.value);
   };
 
   const handleGuess = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (wrongAnswers.length >= 3) return;
+    if (gameOver) return;
 
     if (e.key === "Enter") {
       if (board.find((el) => el.answer === guess)) {
@@ -71,46 +73,67 @@ function FamilyFeud() {
           return el;
         });
         setBoard(newBoard);
+        isGameOver(newBoard, wrongAnswers);
       } else {
-        setWrongAnswers([...wrongAnswers, guess]);
+        const newWrongs = [...wrongAnswers, guess];
+        setWrongAnswers(newWrongs);
+        isGameOver(board, newWrongs);
       }
 
       setGuess("");
     }
   };
 
+  const isGameOver = (board: answer[], wrongAnswers: string[]) => {
+    if (
+      !(wrongAnswers.length < 3 && board.some((el) => el.guessed === false))
+    ) {
+      //the game stops
+      setGameOver(true);
+    }
+  };
+
+  const playAgain = () => {
+    setGuess("");
+    setBoard(prompt.answers.map(el => ({... el })));
+    setGameOver(false);
+    setWrongAnswers([]);
+  }
+
   return (
     <div>
-      <div>
-        <div className="board">
-          <h3>{prompt.question}</h3>
-          <div
-            className="answers"
-            style={{ gridAutoRows: autoRows[board.length] }}
-          >
-            {board.map((el, i) =>
-              el.guessed ? (
-                <div key={i}>
-                  <span>{el.answer}</span> <span>{el.score}</span>
-                </div>
-              ) : (
-                <div key={i}>
-                  <span className="not-guessed">{el.answer}</span>{" "}
-                  <span className="not-guessed">{el.score}</span>
-                </div>
-              )
-            )}
-          </div>
-        </div>
-        <div>
-          {wrongAnswers.map((wrongAnswer, i) => (
-            <div key={i} style={{ textDecorationLine: "line-through" }}>
-              {wrongAnswer}
-            </div>
-          ))}
+      <div className="board">
+        <h3>{prompt.question}</h3>
+        <div
+          className="answers"
+          style={{ gridAutoRows: autoRows[board.length] }}
+        >
+          {board.map((el, i) =>
+            el.guessed ? (
+              <div key={i}>
+                <span>{el.answer}</span> <span>{el.score}</span>
+              </div>
+            ) : (
+              <div key={i}>
+                <span className="not-guessed">{el.answer}</span>{" "}
+                <span className="not-guessed">{el.score}</span>
+              </div>
+            )
+          )}
         </div>
       </div>
-      <input onChange={handleInput} onKeyPress={handleGuess} value={guess} />
+      <div className="action">{!gameOver ? (
+        <input onChange={handleInput} onKeyPress={handleGuess} value={guess} />
+      ) : (
+        <Button variant="primary" onClick={playAgain}>Play Again</Button>
+      )}</div>
+      <div className="wrong-answers">
+        {wrongAnswers.map((wrongAnswer, i) => (
+          <div key={i} className="wrong-answer">
+            {wrongAnswer}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
